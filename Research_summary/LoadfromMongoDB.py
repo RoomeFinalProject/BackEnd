aba4cd4a5effd8459f4b23ddf86682bf25484414
Jeongjoon <lee31412718@gmail.com>
2023-12-17 21:29:33 +0900

    dataorder

--- Research_summary/LoadfromMongoDB.py
+++ Research_summary/LoadfromMongoDB.py
@@ -4,6 +4,9 @@ from datetime import datetime
 
 
 def research_summary_from_DB(k = 5):
+    '''
+        날짜를 선정 후, 최근 5개까지 
+    '''
     client = MongoClient("localhost", 27017)
 
     db = client.Research_Summary
@@ -11,11 +14,13 @@ def research_summary_from_DB(k = 5):
     posts = db.posts
 
     research_summary_list = []
-    for post in posts.find({"Date": {"$gte": "2023-11-10", "$lte": "2023-12-17"}}):
+    for post in posts.find({"Date": {"$gte": "2023-11-10", "$lte": "2023-12-17"}}).sort("Date",-1):
         research_summary_list.append(pprint.pformat(post))
         
     recent_research_k = research_summary_list[:k]
     #print(len(recent_research_5))
     return recent_research_k
+if __name__ == '__main__':
+    print(research_summary_from_DB())
     
 
